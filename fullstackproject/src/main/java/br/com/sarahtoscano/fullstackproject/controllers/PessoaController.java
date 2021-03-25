47331e129f128c5d434c85191d5dfb470f5f3bfe
SarahToscano <sarahtoscano@eng.ci.ufpb.br>
2021-03-25 17:36:08 -0300

    Update PessoaController.java

--- fullstackproject/src/main/java/br/com/sarahtoscano/fullstackproject/controllers/PessoaController.java
+++ fullstackproject/src/main/java/br/com/sarahtoscano/fullstackproject/controllers/PessoaController.java
@@ -3,7 +3,6 @@ package br.com.sarahtoscano.fullstackproject.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
