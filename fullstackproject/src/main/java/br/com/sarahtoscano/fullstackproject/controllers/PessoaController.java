b978e12fad9b756cd08229c202ac8d4d0b845a5e
SarahToscano <sarahtoscano@eng.ci.ufpb.br>
2021-03-25 16:45:40 -0300

    controllers

--- fullstackproject/src/main/java/br/com/sarahtoscano/fullstackproject/controllers/PessoaController.java
+++ fullstackproject/src/main/java/br/com/sarahtoscano/fullstackproject/controllers/PessoaController.java
@@ -1,5 +1,27 @@
 package br.com.sarahtoscano.fullstackproject.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+import br.com.sarahtoscano.fullstackproject.domain.Pessoa;
+import br.com.sarahtoscano.fullstackproject.repository.PessoaRepository;
+
+
+@RestController
+@RequestMapping("/pessoas")
 public class PessoaController {
+
+    @Autowired
+    private PessoaRepository pessoaRepository;
+
+    @GetMapping
+    public List<Pessoa> listarTodos(){
+        return pessoaRepository.findAll();
+    }
     
 }
--- fullstackproject/src/main/java/br/com/sarahtoscano/fullstackproject/repository/PessoaRepository.java
+++ fullstackproject/src/main/java/br/com/sarahtoscano/fullstackproject/repository/PessoaRepository.java
@@ -1,9 +1,7 @@
 package br.com.sarahtoscano.fullstackproject.repository;
-import javax.persistence.Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.sarahtoscano.fullstackproject.domain.Pessoa;
 
-@Entity
 public interface PessoaRepository  extends JpaRepository <Pessoa, Long> {
     
 }
