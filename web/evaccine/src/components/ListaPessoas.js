542723fe504ca085b9d5fed134b4b90b7a5c0411
SarahToscano <sarahtoscano@eng.ci.ufpb.br>
2021-03-27 17:37:49 -0300

    web finalizado

---
+++ web/evaccine/src/components/ListaPessoas.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import {Table, Tag, Space, Button, message} from 'antd';
+import PessoaDataService from "../services/PessoaDataService";
+const {Column} = Table
+
+export default class ListaPessoas extends Component{
+
+    constructor (props){
+        super(props)
+        this.state={
+            pessoas:[],
+            message: null
+        }
+    }
+
+    componentDidMount(){
+        this.refreshPessoas();
+    }
+
+    refreshPessoas(){
+        PessoaDataService.retriveAllPessoas()
+            .then(
+                response => {
+                    console.log(response);
+                    this.setState({pessoas:response.data})
+                }
+            )
+    }
+
+    successUpdate= (record)=>{
+        if(record.isVacinada){
+            record.isVacinada=false;
+        }
+        else{
+            record.isVacinada=true;
+        }
+        PessoaDataService.updatePessoa(record, record.codigo);
+        message.success('Atualização realizada com sucesso.')
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <h2>Pessoas Cadastradas</h2>
+                
+                <div className="container">
+                    <Table dataSource={this.state.pessoas}>
+                        <Column title="Nome" dataIndex="nome" key="nome"/>
+                        <Column title="CPF" dataIndex="cpf" key="cpf"/>
+                        <Column title="Telefone" dataIndex="telefone" key="telefone"/>
+                        <Column title="Email" dataIndex="email" key="email"/>
+                        <Column title="Vacinada" dataIndex="isVacinada" key="isVacinada"
+                                render = {(text, record) => (<p>{String(record.isVacinada)}</p>)}/>
+                        <Column title="Atualizar" key="atualizar"
+                                render = {(text, record) => (<Button onClick={()=> this.successUpdate(record)} type="primary">Alterar status</Button>)}/>
+
+                    </Table>
+                </div>
+            </div>
+        )
+    }
+}
